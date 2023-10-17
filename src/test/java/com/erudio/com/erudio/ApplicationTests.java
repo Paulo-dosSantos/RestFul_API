7d91318aefbf1fb4be04aeed86da212222339809
Paulo Henrique <paulo.13.henrique800@gmail.com>
2023-10-17 16:51:53 -0300

    projeto criado

---
+++ src/test/java/com/erudio/com/erudio/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erudio.com.erudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
